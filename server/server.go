8926a8d8648442a8e0bca521121c8a8364bc1289
capossele <angelocapossele@gmail.com>
2021-07-15 18:36:47 +0100

    add some fixes

--- server/server.go
+++ server/server.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"sync"
+
 	"github.com/iotaledger/goshimmer/packages/tangle"
+	"go.uber.org/atomic"
 
 	"github.com/iotaledger/hive.go/events"
 	"github.com/iotaledger/hive.go/logger"
@@ -14,14 +17,16 @@ import (
 )
 
 type Connection struct {
-	bufferedConn  *buffconn.BufferedConnection
-	log           *logger.Logger
-	active 	      bool
+	bufferedConn *buffconn.BufferedConnection
+	log          *logger.Logger
+	active       bool
 }
 
-
-var connectionList = [256]Connection{}
-var index = 0
+var (
+	connectionList      = [256]Connection{}
+	connectionListMutex sync.RWMutex
+	index               atomic.Uint32
+)
 
 // Listen starts a TCP listener and starts a Connection for each accepted connection
 func Listen(bindAddress string, log *logger.Logger, shutdownSignal <-chan struct{}) error {
@@ -46,16 +51,19 @@ func Listen(bindAddress string, log *logger.Logger, shutdownSignal <-chan struct
 		defer func(listener net.Listener) {
 			err := listener.Close()
 			if err != nil {
-				log.Infof("Error closing listener: %s",err)
+				log.Infof("Error closing listener: %s", err)
 			}
 		}(listener)
 
 		<-shutdownSignal
 
 		log.Infof("Closing Broadcast server...")
-		for i := 0; i < index; i++ {
+		idx := int(index.Load())
+		connectionListMutex.Lock()
+		for i := 0; i < idx; i++ {
 			connectionList[i].active = false
 		}
+		connectionListMutex.Unlock()
 		log.Infof("Closing Broadcast server... \tDone")
 	}()
 
@@ -63,29 +71,33 @@ func Listen(bindAddress string, log *logger.Logger, shutdownSignal <-chan struct
 }
 
 func handleConnection(conn net.Conn, log *logger.Logger, shutdownSignal <-chan struct{}) {
+	connectionListMutex.Lock()
+	defer connectionListMutex.Unlock()
 
-	connectionList[index] = Connection{
-		bufferedConn:  buffconn.NewBufferedConnection(conn, tangle.MaxMessageSize),
-		log:           log,
-		active: 	true,
+	idx := int(index.Load())
+	connectionList[idx] = Connection{
+		bufferedConn: buffconn.NewBufferedConnection(conn, tangle.MaxMessageSize),
+		log:          log,
+		active:       true,
 	}
-	index++
-	bufferedConnDataReceived, bufferedConnClosed := connectionList[index-1].readLoop()
+	index.Inc()
+
+	bufferedConnDataReceived, bufferedConnClosed := connectionList[idx-1].readLoop()
 
 	select {
 	case data := <-bufferedConnDataReceived:
 		//No input required. For debugging it will be printed
-		connectionList[index-1].log.Infof("Data received:%v",data)
-		connectionList[index-1].active = false
+		connectionList[idx-1].log.Infof("Data received:%v", data)
+		connectionList[idx-1].active = false
 	case <-shutdownSignal:
-		connectionList[index-1].log.Infof("Shutdown signal received")
-		for i := 0; i < index; i++ {
+		connectionList[idx-1].log.Infof("Shutdown signal received")
+		for i := 0; i < idx; i++ {
 			connectionList[i].active = false
 		}
 		return
 	case <-bufferedConnClosed:
-		connectionList[index-1].log.Errorf("Connection lost")
-		connectionList[index-1].active = false
+		connectionList[idx-1].log.Errorf("Connection lost")
+		connectionList[idx-1].active = false
 		return
 	}
 }
@@ -123,27 +135,31 @@ func (connection *Connection) readLoop() (chan []byte, chan bool) {
 }
 
 func Broadcast(data []byte) {
-	for i := 0; i < index; i++ {
-		if connectionList[i].active{
+	connectionListMutex.Lock()
+	defer connectionListMutex.Unlock()
+
+	idx := int(index.Load())
+	for i := 0; i < idx; i++ {
+		if connectionList[i].active {
 			_, err := connectionList[i].bufferedConn.Write(data)
 			if err != nil {
-				connectionList[i].log.Debugf("Error writing on connection: %s",err)
+				connectionList[i].log.Debugf("Error writing on connection: %s", err)
 				connectionList[i].active = false
-				return
+				continue
 			}
 		}
 	}
 	//Tidy up array of unused connections
-	if index == 250{
+	if idx >= 250 {
 		newIndex := 0
 		var newConnectionList = [256]Connection{}
-		for i := 0; i < index; i++ {
-			if connectionList[i].active{
+		for i := 0; i < idx; i++ {
+			if connectionList[i].active {
 				newConnectionList[newIndex] = connectionList[i]
 				newIndex++
 			}
 		}
-		index = newIndex
+		index.Store(uint32(newIndex))
 		connectionList = newConnectionList
 	}
 }

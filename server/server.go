876fab8b25ad5120528d3e901c43a9295f192e55
Arne Fuchs <arnefuchs@protonmail.com>
2021-09-06 09:03:57 +0200

    Bug fixes and improvements

--- server/server.go
+++ server/server.go
@@ -1,36 +1,35 @@
 package server
 
 import (
+	"github.com/cockroachdb/errors"
+	"github.com/iotaledger/hive.go/node"
 	"sync"
 
 	"github.com/iotaledger/goshimmer/packages/tangle"
 	"go.uber.org/atomic"
 
 	"github.com/iotaledger/hive.go/events"
-	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/hive.go/netutil/buffconn"
 
 	"fmt"
 	"io"
 	"net"
-	"strings"
 )
 
-type Connection struct {
+type connection struct {
 	bufferedConn *buffconn.BufferedConnection
-	log          *logger.Logger
+	log          *node.Plugin //Is just used for logging
 	active       bool
 }
 
 var (
-	connectionList      = [256]Connection{}
+	connectionList      = [256]connection{}
 	connectionListMutex sync.RWMutex
 	index               atomic.Uint32
 )
 
 // Listen starts a TCP listener and starts a Connection for each accepted connection
-func Listen(bindAddress string, log *logger.Logger, shutdownSignal <-chan struct{}) error {
-
+func Listen(bindAddress string, log *node.Plugin, shutdownSignal <-chan struct{}) error {
 	listener, err := net.Listen("tcp", bindAddress)
 	if err != nil {
 		return fmt.Errorf("failed to start Broadcast daemon: %w", err)
@@ -40,92 +39,88 @@ func Listen(bindAddress string, log *logger.Logger, shutdownSignal <-chan struct
 		for {
 			connection, err := listener.Accept()
 			if err != nil {
+				if connection != nil {
+					log.LogInfof("Couldn't accept connection: %s", err)
+				}
 				return
 			}
-			log.Infof("Started connection: %s", connection.RemoteAddr().String())
+			log.LogDebugf("Started connection: %s", connection.RemoteAddr().String())
 			go handleConnection(connection, log, shutdownSignal)
 		}
 	}()
 
 	go func() {
 		defer func(listener net.Listener) {
-			err := listener.Close()
-			if err != nil {
-				log.Infof("Error closing listener: %s", err)
+			if listener.Close() != nil {
+				log.LogInfof("Error closing listener: %s", err)
 			}
 		}(listener)
 
 		<-shutdownSignal
 
-		log.Infof("Closing Broadcast server...")
+		log.LogInfof("Closing Broadcast server...")
 		idx := int(index.Load())
-		connectionListMutex.Lock()
 		for i := 0; i < idx; i++ {
 			connectionList[i].active = false
 		}
-		connectionListMutex.Unlock()
-		log.Infof("Closing Broadcast server... \tDone")
+		removeInactiveConnections()
+		log.LogInfof("Closing Broadcast server... done")
 	}()
 
 	return nil
 }
 
-func handleConnection(conn net.Conn, log *logger.Logger, shutdownSignal <-chan struct{}) {
+func handleConnection(conn net.Conn, log *node.Plugin, shutdownSignal <-chan struct{}) {
 	connectionListMutex.Lock()
-	defer connectionListMutex.Unlock()
 
 	idx := int(index.Load())
-	connectionList[idx] = Connection{
+	connectionList[idx] = connection{
 		bufferedConn: buffconn.NewBufferedConnection(conn, tangle.MaxMessageSize),
 		log:          log,
 		active:       true,
 	}
 	index.Inc()
+	connectionListMutex.Unlock()
 
-	bufferedConnDataReceived, bufferedConnClosed := connectionList[idx-1].readLoop()
+	bufferedConnDataReceived, bufferedConnClosed := connectionList[idx].readLoop()
 
 	select {
 	case data := <-bufferedConnDataReceived:
 		//No input required. For debugging it will be printed
-		connectionList[idx-1].log.Infof("Data received:%v", data)
-		connectionList[idx-1].active = false
+		connectionList[idx].log.LogDebugf("Data received:%v", data)
 	case <-shutdownSignal:
-		connectionList[idx-1].log.Infof("Shutdown signal received")
-		for i := 0; i < idx; i++ {
-			connectionList[i].active = false
-		}
-		return
+		connectionList[idx].log.LogInfof("Shutdown signal received")
+		connectionList[idx].active = false
 	case <-bufferedConnClosed:
-		connectionList[idx-1].log.Errorf("Connection lost")
-		connectionList[idx-1].active = false
-		return
+		connectionList[idx].log.LogDebugf("Connection lost")
+		connectionList[idx].active = false
 	}
 }
 
-func (connection *Connection) readLoop() (chan []byte, chan bool) {
+func (connection *connection) readLoop() (chan []byte, chan bool) {
 	bufferedConnDataReceived := make(chan []byte)
 	bufferedConnClosed := make(chan bool)
 
 	go func() {
 		{
-			cl := events.NewClosure(func() { close(bufferedConnClosed) })
-			connection.bufferedConn.Events.Close.Attach(cl)
-			defer connection.bufferedConn.Events.Close.Detach(cl)
+			connectionClosedClosure := events.NewClosure(func() { close(bufferedConnClosed) })
+			connection.bufferedConn.Events.Close.Attach(connectionClosedClosure)
+			defer connection.bufferedConn.Events.Close.Detach(connectionClosedClosure)
 		}
 
 		{
-			cl := events.NewClosure(func(data []byte) {
+			connectionDataReceivedClosure := events.NewClosure(func(data []byte) {
 				d := make([]byte, len(data))
 				copy(d, data)
 				bufferedConnDataReceived <- d
 			})
-			connection.bufferedConn.Events.ReceiveMessage.Attach(cl)
-			defer connection.bufferedConn.Events.ReceiveMessage.Detach(cl)
+			connection.bufferedConn.Events.ReceiveMessage.Attach(connectionDataReceivedClosure)
+			defer connection.bufferedConn.Events.ReceiveMessage.Detach(connectionDataReceivedClosure)
 		}
 
 		if err := connection.bufferedConn.Read(); err != nil {
-			if err != io.EOF && !strings.Contains(err.Error(), "Use of closed network connection") {
-				connection.log.Warnw("Buffered connection read error", "err", err)
+			if err != io.EOF && errors.Is(err, net.ErrClosed) {
+				connection.log.LogDebugf("Buffered connection read error", "err", err)
 				connection.active = false
 			}
 		}
@@ -141,18 +136,21 @@ func Broadcast(data []byte) {
 	idx := int(index.Load())
 	for i := 0; i < idx; i++ {
 		if connectionList[i].active {
-			_, err := connectionList[i].bufferedConn.Write(data)
-			if err != nil {
-				connectionList[i].log.Debugf("Error writing on connection: %s", err)
+			if _, err := connectionList[i].bufferedConn.Write(data); err != nil {
+				connectionList[i].log.LogInfof("Error writing on connection: %s", err)
 				connectionList[i].active = false
-				continue
 			}
 		}
 	}
 	//Tidy up array of unused connections
+	removeInactiveConnections()
+}
+
+func removeInactiveConnections() {
+	idx := int(index.Load())
 	if idx >= 250 {
 		newIndex := 0
-		var newConnectionList = [256]Connection{}
+		var newConnectionList = [256]connection{}
 		for i := 0; i < idx; i++ {
 			if connectionList[i].active {
 				newConnectionList[newIndex] = connectionList[i]
